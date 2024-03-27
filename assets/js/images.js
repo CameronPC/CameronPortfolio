6e1e6880707107039737ae2b5557c33ca5b36049
CameronPC <cameroncummings@u.boisestate.edu>
2024-03-27 13:47:41 -0600

    new carosel

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,31 +1,19 @@
-	//Slideshow
-	let slideIndex = 0;
-	showSlides();
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-	function plusSlides(n) {
-        console.log("cock");
-		showSlides(slideIndex += n);
-	  }
-	  
-	  function currentSlide(n) {
-		showSlides(slideIndex = n);
-	  }
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
 
-	function showSlides() {
-		let i;
-		let slides = document.getElementsByClassName("mySlides");
-		let dots = document.getElementsByClassName("dot");
-		console.log(slides.length);
-		for (i = 0; i < slides.length/2; i++) {
-			slides[i].style.display = "none";
-		}
-		console.log(slides.length);
-		slideIndex++;
-		if (slideIndex > slides.length/2) { slideIndex = 1 }
-		for (i = 0; i < dots.length; i++) {
-			dots[i].className = dots[i].className.replace(" active", "");
-		}
-		slides[slideIndex - 1].style.display = "block";
-		dots[slideIndex - 1].className += " active";
-		setTimeout(showSlides, 5000); // Change image every 5 seconds
-	}
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
